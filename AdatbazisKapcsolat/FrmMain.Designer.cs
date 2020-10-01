7b6e1df13d60a5d424b9f2ec0dc34b840a1008d1
Balázs Jáger <notaxmart@gmail.com>
2020-10-01 19:25:48 +0200

    modositas

--- AdatbazisKapcsolat/FrmMain.Designer.cs
+++ AdatbazisKapcsolat/FrmMain.Designer.cs
@@ -29,38 +29,37 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.dgw_adatok = new System.Windows.Forms.DataGridView();
+            this.tableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet2 = new AdatbazisKapcsolat.Database1DataSet2();
             this.database1DataSetBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.database1DataSet = new AdatbazisKapcsolat.Database1DataSet();
             this.database1DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.database1DataSetBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
-            this.database1DataSet2 = new AdatbazisKapcsolat.Database1DataSet2();
-            this.tableBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.tableTableAdapter = new AdatbazisKapcsolat.Database1DataSet2TableAdapters.TableTableAdapter();
-            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.nevDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btn_load = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dgw_adatok)).BeginInit();
+            this.dgw_adatok = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nev = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.tb_input = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSetBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSetBindingSource2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_adatok)).BeginInit();
             this.SuspendLayout();
             // 
-            // dgw_adatok
+            // tableBindingSource
             // 
-            this.dgw_adatok.AutoGenerateColumns = false;
-            this.dgw_adatok.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgw_adatok.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.idDataGridViewTextBoxColumn,
-            this.nevDataGridViewTextBoxColumn});
-            this.dgw_adatok.DataSource = this.tableBindingSource;
-            this.dgw_adatok.Location = new System.Drawing.Point(74, 29);
-            this.dgw_adatok.Name = "dgw_adatok";
-            this.dgw_adatok.Size = new System.Drawing.Size(468, 380);
-            this.dgw_adatok.TabIndex = 0;
+            this.tableBindingSource.DataMember = "Table";
+            this.tableBindingSource.DataSource = this.database1DataSet2;
+            // 
+            // database1DataSet2
+            // 
+            this.database1DataSet2.DataSetName = "Database1DataSet2";
+            this.database1DataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // database1DataSetBindingSource1
             // 
@@ -82,32 +81,9 @@
             this.database1DataSetBindingSource2.DataSource = this.database1DataSet;
             this.database1DataSetBindingSource2.Position = 0;
             // 
-            // database1DataSet2
-            // 
-            this.database1DataSet2.DataSetName = "Database1DataSet2";
-            this.database1DataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // tableBindingSource
-            // 
-            this.tableBindingSource.DataMember = "Table";
-            this.tableBindingSource.DataSource = this.database1DataSet2;
-            // 
             // tableTableAdapter
             // 
             this.tableTableAdapter.ClearBeforeFill = true;
-            // 
-            // idDataGridViewTextBoxColumn
-            // 
-            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
-            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
-            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
-            // 
-            // nevDataGridViewTextBoxColumn
-            // 
-            this.nevDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.nevDataGridViewTextBoxColumn.DataPropertyName = "nev";
-            this.nevDataGridViewTextBoxColumn.HeaderText = "nev";
-            this.nevDataGridViewTextBoxColumn.Name = "nevDataGridViewTextBoxColumn";
             // 
             // btn_load
             // 
@@ -119,30 +95,69 @@
             this.btn_load.UseVisualStyleBackColor = true;
             this.btn_load.Click += new System.EventHandler(this.Btn_load_Click);
             // 
-            // Form1
+            // dgw_adatok
+            // 
+            this.dgw_adatok.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw_adatok.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.nev});
+            this.dgw_adatok.Location = new System.Drawing.Point(74, 29);
+            this.dgw_adatok.Name = "dgw_adatok";
+            this.dgw_adatok.Size = new System.Drawing.Size(468, 380);
+            this.dgw_adatok.TabIndex = 0;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "Id";
+            this.id.Name = "id";
+            // 
+            // nev
+            // 
+            this.nev.HeaderText = "nev";
+            this.nev.Name = "nev";
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(89, 454);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(168, 63);
+            this.btn_add.TabIndex = 2;
+            this.btn_add.Text = "Hozzaad";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.Btn_add_Click);
+            // 
+            // tb_input
+            // 
+            this.tb_input.Location = new System.Drawing.Point(89, 428);
+            this.tb_input.Name = "tb_input";
+            this.tb_input.Size = new System.Drawing.Size(100, 20);
+            this.tb_input.TabIndex = 3;
+            // 
+            // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(728, 529);
+            this.Controls.Add(this.tb_input);
+            this.Controls.Add(this.btn_add);
             this.Controls.Add(this.btn_load);
             this.Controls.Add(this.dgw_adatok);
-            this.Name = "Form1";
+            this.Name = "FrmMain";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dgw_adatok)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSetBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSetBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSetBindingSource2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_adatok)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dgw_adatok;
         private System.Windows.Forms.BindingSource database1DataSetBindingSource;
         private Database1DataSet database1DataSet;
         private System.Windows.Forms.BindingSource database1DataSetBindingSource1;
@@ -150,9 +165,12 @@
         private Database1DataSet2 database1DataSet2;
         private System.Windows.Forms.BindingSource tableBindingSource;
         private Database1DataSet2TableAdapters.TableTableAdapter tableTableAdapter;
-        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn nevDataGridViewTextBoxColumn;
         private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.DataGridView dgw_adatok;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nev;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.TextBox tb_input;
     }
 }
 
--- AdatbazisKapcsolat/FrmMain.cs
+++ AdatbazisKapcsolat/FrmMain.cs
@@ -27,7 +27,7 @@ namespace AdatbazisKapcsolat
         private void Form1_Load(object sender, EventArgs e)
         {
             // TODO: This line of code loads data into the 'database1DataSet2.Table' table. You can move, or remove it, as needed.
-            this.tableTableAdapter.Fill(this.database1DataSet2.Table);
+
 
         }
 
@@ -36,8 +36,30 @@ namespace AdatbazisKapcsolat
 
             conn.Open();
 
+            var cmd = new SqlCommand("select * from proba", conn);
+
+            var r = cmd.ExecuteReader();
+
+            while (r.Read())
+            {
+                dgw_adatok.Rows.Add(r[0], r[1]);
+            }
 
             conn.Close();
+
+        }
+
+        private void Btn_add_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            var adapter = new SqlDataAdapter(){
+                InsertCommand = new SqlCommand($"insert into proba (nev) VALUES ('{tb_input.Text}');", conn),
+        };
+            adapter.InsertCommand.ExecuteNonQuery();
+            conn.Close();
+
+          
 
         }
     }
